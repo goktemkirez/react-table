a180f6f13c3e2dfcbe8d9734df56baca306f2f03
goktemkirez <ogkirez@gmail.com>
2024-05-15 14:48:00 +0300

    table test with dummy json

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,59 @@
 import React from 'react'
-
-import { ExampleComponent } from '@goktemkirez/react-table'
 import '@goktemkirez/react-table/dist/index.css'
+// import { ExampleComponent } from '@goktemkirez/react-table'
+import Table from '@goktemkirez/react-table'
+
+import dummyJSON from "./dummy-json"
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <>
+    {/* <ExampleComponent text="Create React Library Example 😄" /> */}
+    <Table 
+        title="Test Table"
+        data={dummyJSON}
+        columns={[
+          {
+            name: "dummyId",
+            title: "dummyId",
+            type: "number",
+          },
+          {
+            name: "dummyText",
+            title: "dummyText",
+            type: "string",
+          },
+          {
+            name: "dummyDate",
+            title: "dummyDate",
+            type: "date",
+          },
+          {
+            name: "dummyNumber",
+            title: "dummyNumber",
+            type: "number",
+          },
+          {
+            name: "dummyNumberWithDot",
+            title: "dummyNumberWithDot",
+            type: "number",
+          },
+          {
+            name: "dummyBool",
+            title: "dummyBool",
+            type: "bool",
+          },
+          {
+            name: "dummyBoolNumber",
+            title: "dummyBoolNumber",
+            type: "bool",
+          },
+          {
+            name: "dummyUrl",
+            title: "dummyUrl",
+            type: "string",
+          },
+        ]} />
+  </>
 }
 
 export default App
---
+++ example/src/dummy-json.js
@@ -0,0 +1,28 @@
+const dummyJSON = [
+    {
+        "dummyId": 1,
+        "dummyText": "Proses Yazılım",
+        "dummyDate": "2024-05-15T12:58:33.387",
+        "dummyNumber": 83,
+        "dummyNumberWithDot": 83.95,
+        "dummyBool": true,
+        "dummyBoolNumber": 1,
+        "dummyUrl": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+        "dummyArray": ["text1", "text2", "text3"],
+        "dummyObject": {"dummyProp1": "text1", "dummyProp2": "text2"}
+    },
+    {
+        "dummyId": 1,
+        "dummyText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est leo, fringilla a tellus in, luctus facilisis erat. Cras eget nulla eu magna maximus ultrices in et ex. In hac habitasse platea dictumst. Proin pharetra euismod aliquam. Suspendisse mattis pretium tortor vel ultricies. Morbi blandit interdum efficitur. Maecenas hendrerit sem metus, vitae rutrum dolor dictum nec. Suspendisse gravida augue quis elit semper malesuada non vitae justo.",
+        "dummyDate": "2024-05-15T12:58:33.387",
+        "dummyNumber": -83,
+        "dummyNumberWithDot": -83.95,
+        "dummyBool": false,
+        "dummyBoolNumber": 0,
+        "dummyUrl": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+        "dummyArray": [1, 2, 3],
+        "dummyObject": {"dummyProp1": 1, "dummyProp2": 2}
+    },
+]
+
+export default dummyJSON
